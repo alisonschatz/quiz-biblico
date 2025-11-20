7bbeff17c32a07d978b9af3bce483dda832d81d2
Alison Schatz <alisonschatz1@gmail.com>
2025-11-20 03:28:05 -0300

    ⬆️ chore: upgrade dependencies and update versions

--- quasar.config.js
+++ quasar.config.js
@@ -1,55 +1,75 @@
-const { configure } = require('quasar/wrappers');
+import { defineConfig } from '#q-app/wrappers'
 
-module.exports = configure(function (ctx) {
+export default defineConfig((ctx) => {
   return {
     boot: ['firebase'],
-
+    
     css: ['app.scss'],
-
+    
     extras: [
       'roboto-font',
       'material-icons',
       'material-icons-outlined'
     ],
-
+    
     build: {
       target: {
-        browser: ['es2019'],
+        browser: ['es2022', 'edge88', 'firefox78', 'chrome87', 'safari14'],
         node: 'node18'
       },
-
+      
       vueRouterMode: 'hash',
+      
       vitePlugins: []
     },
-
+    
     devServer: {
-      open: true,      
+      open: true
     },
-
+    
     framework: {
       config: {},
-      plugins: ['Notify', 'Dialog', 'Loading']
+      
+      plugins: [
+        'Notify',
+        'Loading',
+        'Dialog'
+      ]
     },
-
+    
     animations: 'all',
-
+    
+    ssr: {
+      pwa: false,
+      prodPort: 3000,
+      middlewares: [ctx.prod ? 'compression' : '', 'render']
+    },
+    
     pwa: {
       workboxMode: 'generateSW',
       injectPwaMetaTags: true,
       swFilename: 'sw.js',
-      manifestFilename: 'manifest.json'
+      manifestFilename: 'manifest.json',
+      useCredentialsForManifestTag: false
     },
-
+    
+    cordova: {},
+    
     capacitor: {
       hideSplashscreen: true
     },
-
+    
     electron: {
       inspectPort: 5858,
-      bundler: 'builder',
+      bundler: 'packager',
+      packager: {},
       builder: {
         appId: 'quiz-biblico'
       }
+    },
+    
+    bex: {
+      contentScripts: ['my-content-script']
     }
-  };
-});
+  }
+})
